a9e3f4ff86c1221a701656454072d0e39ea7113b
mtj0928 <mtj0928@users.noreply.github.com>
2020-06-26 09:45:27 +0900

    Update Project Structure

--- Package.swift
+++ Package.swift
@@ -6,17 +6,26 @@ import PackageDescription
 let package = Package(
     name: "SwiftyClone",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/kylef/Commander.git", from: "0.0.0"),
+        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("0.50200.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SwiftyClone",
-            dependencies: []),
+            name: "SwiftyCloneCLI",
+            dependencies: [
+                "Commander",
+                "SwiftyCloneCore"
+            ]
+        ),
+        .target(
+            name: "SwiftyCloneCore",
+            dependencies: [
+                "SwiftSyntax",
+            ]
+        ),
         .testTarget(
             name: "SwiftyCloneTests",
-            dependencies: ["SwiftyClone"]),
-    ]
+            dependencies: ["SwiftyCloneCore"]),
+    ],
+    swiftLanguageVersions: [.v5]
 )
